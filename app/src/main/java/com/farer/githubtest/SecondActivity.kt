c573092c2760d6345083bba9cbf5d5114df3c506
poemfarer <poemfarer@gmail.com>
2024-03-11 13:42:42 +0900

    view binding withe SecondActivity

--- app/src/main/java/com/farer/githubtest/SecondActivity.kt
+++ app/src/main/java/com/farer/githubtest/SecondActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.farer.githubtest.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,7 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding= ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
